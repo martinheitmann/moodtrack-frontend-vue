c9075f002c233f186cdd6e27968a9bd760a70fc6
martinheitmann <mart.hei@live.com>
2022-02-09 13:16:15 +0100

    Split questionnaire creator components + bug fixes

--- src/views/in_app_questionnaire/InAppQuestionnaire.vue
+++ src/views/in_app_questionnaire/InAppQuestionnaire.vue
@@ -156,184 +156,11 @@
                 </CButton>
               </CCol>
             </CRow>
-            <CRow class="mt-2" v-if="newQuestionMode">
-              <CCol>
-                <CCard>
-                  <CCardBody>
-                    <CCol>
-                      <CRow>
-                        New Question
-                      </CRow>
-                      <CRow class="mt-2">
-                        <CCol xs class="mr-2">
-                          <CButton
-                            v-bind="buttonVariant1"
-                            :color="'primary'"
-                            v-on:click="switchNewQuestionType('FREE_TEXT')"
-                          >
-                            Free-text
-                          </CButton>
-                        </CCol>
-                        <CCol xs>
-                          <CButton
-                            v-bind="buttonVariant2"
-                            :color="'primary'"
-                            v-on:click="switchNewQuestionType('MULTI_CHOICE')"
-                          >
-                            Multiple Choice
-                          </CButton>
-                        </CCol>
-                      </CRow>
-                      <CRow v-if="newQuestionType === 'FREE_TEXT'" class="mt-2">
-                        <CCol>
-                          <CRow>
-                            <CInput
-                              class="w-100"
-                              v-model="newFreeTextQuestionTextInput"
-                              placeholder="Provide your question text..."
-                            />
-                          </CRow>
-                        </CCol>
-                      </CRow>
-                      <CRow
-                        v-if="newQuestionType === 'MULTI_CHOICE'"
-                        class="mt-2"
-                      >
-                        <CCol>
-                          <CRow>
-                            <CInput
-                              class="w-100"
-                              v-model="newFreeTextQuestionTextInput"
-                              placeholder="Provide your question text..."
-                            />
-                          </CRow>
-
-                          <CRow>
-                            <CCol>
-                              <CRow
-                                v-for="(item, index) in newQuestionChoices"
-                                :key="item._id"
-                                class="mb-2 mt-2 border rounded"
-                                align-vertical="center"
-                              >
-                                <CCol class="m-2">
-                                  {{ item.display || item.value }}
-                                </CCol>
-                                <CCol xs class="m-2">
-                                  <CButton
-                                    class="m-1"
-                                    :size="'sm'"
-                                    :color="'danger'"
-                                    v-on:click="deleteNewQuestionChoice(index)"
-                                  >
-                                    <CIcon :content="$options.X" />
-                                  </CButton>
-                                </CCol>
-                              </CRow>
-                            </CCol>
-                          </CRow>
-
-                          <CRow>
-                            <CButton
-                              :color="'info'"
-                              :size="'sm'"
-                              v-on:click="toggleAddChoiceMode()"
-                            >
-                              <CRow align-vertical="center">
-                                <CCol xs class="mb-2 mt-2 mr-1 ml-3">
-                                  <CIcon :content="$options.Plus" />
-                                </CCol>
-                                <CCol xs class="mb-2 mt-2 ml-1 mr-3">
-                                  Add Alternative
-                                </CCol>
-                              </CRow>
-                            </CButton>
-                          </CRow>
-
-                          <CRow
-                            v-if="
-                              newQuestionType === 'MULTI_CHOICE' &&
-                                addChoiceMode
-                            "
-                            class="mt-2"
-                          >
-                            <CCol>
-                              <CRow>
-                                <CCol>
-                                  New question choice
-                                </CCol>
-                              </CRow>
-                              <CRow>
-                                <CCol>
-                                  <CInput
-                                    v-model="newChoiceValueInput"
-                                    placeholder="Value"
-                                  />
-                                </CCol>
-                              </CRow>
-                              <CRow>
-                                <CCol>
-                                  <CInput
-                                    v-model="newChoiceDisplayInput"
-                                    placeholder="Display (Optional)"
-                                  />
-                                </CCol>
-                              </CRow>
-                              <CRow>
-                                <CCol>
-                                  <Multiselect
-                                    v-model="newChoiceTypeInput"
-                                    :options="choiceTypeOptions"
-                                    label="display"
-                                    track-by="value"
-                                    placeholder="Type (Optional)"
-                                  />
-                                </CCol>
-                              </CRow>
-                              <CRow class="mt-2" align-horizontal="start">
-                                <CCol xs class="m-2">
-                                  <CButton
-                                    :color="'success'"
-                                    v-on:click="addNewChoice()"
-                                  >
-                                    Add Choice
-                                  </CButton>
-                                </CCol>
-                                <CCol xs class="m-2">
-                                  <CButton
-                                    :color="'danger'"
-                                    v-on:click="toggleAddChoiceMode()"
-                                  >
-                                    Close
-                                  </CButton>
-                                </CCol>
-                              </CRow>
-                            </CCol>
-                          </CRow>
-                        </CCol>
-                      </CRow>
-                      <CRow class="mt-2" align-horizontal="end">
-                        <CCol xs class="mr-2 mt-2 mb-2">
-                          <CButton
-                            :color="'success'"
-                            v-on:click="addNewQuestion()"
-                          >
-                            Add Question
-                          </CButton>
-                        </CCol>
-                        <CCol xs class="ml-2 mt-2 mb-2">
-                          <CButton
-                            :color="'danger'"
-                            v-on:click="toggleNewQuestionMode()"
-                          >
-                            Cancel
-                          </CButton>
-                        </CCol>
-                      </CRow>
-                    </CCol>
-                  </CCardBody>
-                </CCard>
-              </CCol>
+            <CRow v-if="newQuestionMode">
+              <CreateQuestionnaireItem
+                :onClose="toggleNewQuestionMode"
+                :onSubmitNewQuestion="addNewQuestion"
+              />
             </CRow>
           </CCol>
         </CCardBody>
@@ -394,9 +221,18 @@ export default {
       import("./list_components/InAppQuestionnaireFreeText"),
     InAppQuestionnaireMultiChoice: () =>
       import("./list_components/InAppQuestionnaireMultiChoice"),
-    Multiselect: () => import("vue-multiselect"),
     InAppQuestionnaireContentRevisions: () =>
       import("./InAppQuestionnaireContentRevisions"),
+    CreateQuestionnaireItem: () =>
+      import("./list_components/CreateQuestionnaireItem"),
+  },
+  watch: {
+    newQuestionMode: {
+      immediate: true,
+      handler(val) {
+        console.log("newQuestionMode lis: " + val);
+      },
+    },
   },
   data() {
     return {
@@ -412,21 +248,6 @@ export default {
       //Modes
       newQuestionMode: false,
       addChoiceMode: false,
-      //Common
-      newFreeTextQuestionTextInput: "",
-      //Multi choice
-      newQuestionChoices: [],
-      newQuestionType: "FREE_TEXT",
-      newChoiceTypeInput: null,
-      newChoiceValueInput: "",
-      newChoiceDisplayInput: null,
-      // Button styling
-      buttonVariant1: {},
-      buttonVariant2: { variant: "outline" },
-      choiceTypeOptions: [
-        { display: "Number", value: "NUMBER" },
-        { display: "Text", value: "TEXT" },
-      ],
       errorMessage: "",
       successMessage: "",
       errorToasts: 0,
@@ -471,29 +292,38 @@ export default {
     deleteQuestion(index) {
       this.inAppQuestionnaireListItems.splice(index, 1);
     },
-    deleteNewQuestionChoice(index) {
-      this.newQuestionChoices.splice(index, 1);
-    },
-    createNewFreeTextQuestion() {
+    createNewFreeTextQuestion({ newFreeTextQuestionTextInput }) {
       return {
         type: ItemType.FREE_TEXT,
-        question: this.newFreeTextQuestionTextInput,
+        question: newFreeTextQuestionTextInput,
       };
     },
-    createNewMultiChoiceQuestion() {
+    createNewMultiChoiceQuestion({
+      newFreeTextQuestionTextInput,
+      newQuestionChoices,
+    }) {
       return {
         type: ItemType.MULTI_CHOICE,
-        question: this.newFreeTextQuestionTextInput,
-        choices: this.newQuestionChoices,
+        question: newFreeTextQuestionTextInput,
+        choices: newQuestionChoices,
       };
     },
-    addNewQuestion() {
+    addNewQuestion({
+      newFreeTextQuestionTextInput,
+      newQuestionChoices,
+      newQuestionType,
+    }) {
       let nc = null;
-      if (this.newQuestionType === "FREE_TEXT") {
-        nc = this.createNewFreeTextQuestion();
+      if (newQuestionType === "FREE_TEXT") {
+        nc = this.createNewFreeTextQuestion({
+          newFreeTextQuestionTextInput,
+        });
       }
-      if (this.newQuestionType === "MULTI_CHOICE") {
-        nc = this.createNewMultiChoiceQuestion();
+      if (newQuestionType === "MULTI_CHOICE") {
+        nc = this.createNewMultiChoiceQuestion({
+          newFreeTextQuestionTextInput,
+          newQuestionChoices,
+        });
       }
       this.$set(
         this.inAppQuestionnaireListItems,
@@ -501,31 +331,6 @@ export default {
         nc
       );
       this.toggleNewQuestionMode();
-    },
-    addNewChoice() {
-      const value = this.newChoiceValueInput;
-      const display = this.newChoiceDisplayInput;
-      const type = this.newChoiceTypeInput?.value;
-      this.$set(this.newQuestionChoices, this.newQuestionChoices.length, {
-        display: display,
-        value: value,
-        type: type,
-      });
-      this.toggleAddChoiceMode();
-    },
-    toggleAddChoiceMode() {
-      this.addChoiceMode = !this.addChoiceMode;
-    },
-    switchNewQuestionType(newType) {
-      this.newQuestionType = newType;
-      if (newType === "FREE_TEXT") {
-        this.buttonVariant1 = {};
-        this.buttonVariant2 = { variant: "outline" };
-      }
-      if (newType === "MULTI_CHOICE") {
-        this.buttonVariant2 = {};
-        this.buttonVariant1 = { variant: "outline" };
-      }
     },
     editItem(item, index) {
       this.$set(this.inAppQuestionnaireListItems, index, item);
